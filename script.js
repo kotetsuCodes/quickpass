234bf5e0a85b06a2c90b5b689d0ba2adcaf7de61
Carl Brown <carl_brown87@outlook.com>
2016-07-14 09:17:24 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const {remote, globalShortcut} = require('electron');
+// Module to handle clipboard
+const {clipboard} = require('electron');
+
+//const {globalShortcut} = require('global-shortcut')
+
+clipboard.writeText('Example String');
+
+
+
